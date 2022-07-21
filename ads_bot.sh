97e70bb4a4246b6f240441bb5da16dbdaa55039b
Merp <theowenyang@gmail.com>
2022-07-21 16:17:08 -0700

    Added an extra and excluding list feature.

--- ads_bot.sh
+++ ads_bot.sh
@@ -10,13 +10,31 @@ do
     
 done
 
-sed 's/\localhost//g;s/\#.*/#/;s/\#//g;s/\::1//g;s/\=//g;' ad_list.txt > ad_list_2.txt
+d=$(sed -n '$=' ad_extra.txt)
 
-awk '{print $NF}'  ad_list_2.txt > ad_list.txt
+for ((w=1; w<=d; w++))
+do
+    ad_extra=$(sed -n "$w p" ad_extra.txt)
+    echo "$ad_extra" >> ad_list.txt
+    
+done
 
-sed 's/ //g;' ad_list.txt > ad_list_2.txt
+e=$(sed -n '$=' ad_excluded.txt)
 
-sort -u ad_list_2.txt > ad_final_list.txt
+for ((f=1; f<=e; f++))
+do
+    excluded=$(sed -n "$f p" ad_extra.txt)
+    sed "s/\<$excluded\>//g" ad_list.txt > ad_list_2.txt
+    
+done
+
+sed 's/\localhost//g;s/\#.*/#/;s/\#//g;s/\::1//g;s/\=//g;' ad_list_2.txt > ad_list.txt
+
+awk '{print $NF}'  ad_list.txt > ad_list_2.txt
+
+sed 's/ //g;' ad_list_2.txt >ad_list.txt
+
+sort -u ad_list.txt > ad_final_list.txt
 
 rm ad_list.txt
 rm ad_list_2.txt
